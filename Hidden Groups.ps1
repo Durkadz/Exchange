0364fbcdfaf3c0333b8f802d2c65a59c1b7ca4fe
Ivan <56428200+Durkadz@users.noreply.github.com>
2019-10-11 09:12:19 +0200

    Add files via upload

---
+++ Hidden Groups.ps1
@@ -0,0 +1,27 @@
+﻿$groups = Get-DistributionGroup * | 
+Where {[String]$_.Name -Match "GRGBes*" -or [String]$_.Name -Match "GrGNB*"}
+Foreach($group In $groups){
+ try{   
+        $Gr = $group.name
+        Set-DistributionGroup $Gr -HiddenFromAddressListsEnabled $true -ErrorAction Stop
+        "$Gr,Ocultado" | Out-File c:\temp\groupsclosed2.txt -Append
+
+    }
+ catch [System.Exception] {
+ "$Gr,Cerrado" | Out-File c:\temp\groupsclosed2.txt -Append
+ Write-Host "Cerrado"
+ }
+}
+
+
+
+    
+        
+    
+   
+
+
+
+
+
+
